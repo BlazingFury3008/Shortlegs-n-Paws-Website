cf81a58e65cc493a3d9cb5744c2c5175a67323e5
Aiden Taylor <ataylor3008@gmail.com>
2026-04-22 03:29:50 +0100

    Privacy text added, to be styled

---
+++ frontend/app/components/privacy.ts
@@ -0,0 +1,90 @@
+export const privacy = `
+Shortlegs N’ Paws customer privacy notice
+This privacy notice tells you what to expect us to do with your personal information.
+* Contact details
+* What information we collect, use, and why
+* Lawful bases and data protection rights
+* Where we get personal information from
+* How long we keep information
+* Who we share information with
+* How to complain
+Contact details
+Telephone
+
+07568073267
+Email
+
+shortlegs_lisa@hotmail.co.uk
+What information we collect, use, and why
+We collect or use the following information to provide services and goods, including delivery:
+* Names and contact details
+* Addresses
+* Payment details (including card or bank information for transfers and direct debits)
+* Photographs or video recordings
+We collect or use the following information for the operation of customer accounts and guarantees:
+* Names and contact details
+* Addresses
+* Payment details (including card or bank information for transfers and direct debits)
+We collect or use the following information for service updates or marketing purposes:
+* Recorded images, such as photos or videos
+Lawful bases and data protection rights
+
+Under UK data protection law, we must have a “lawful basis” for collecting and using your personal information. There is a list of possible lawful bases in the UK GDPR. You can find out more about lawful bases on the ICO’s website.
+
+Which lawful basis we rely on may affect your data protection rights which are set out in brief below. You can find out more about your data protection rights and the exemptions which may apply on the ICO’s website:
+
+* Your right of access - You have the right to ask us for copies of your personal information. You can request other information such as details about where we get personal information from and who we share personal information with. There are some exemptions which means you may not receive all the information you ask for. Read more about the right of access.
+* Your right to rectification - You have the right to ask us to correct or delete personal information you think is inaccurate or incomplete. Read more about the right to rectification.
+* Your right to erasure - You have the right to ask us to delete your personal information. Read more about the right to erasure.
+* Your right to restriction of processing - You have the right to ask us to limit how we can use your personal information. Read more about the right to restriction of processing.
+* Your right to object to processing - You have the right to object to the processing of your personal data. Read more about the right to object to processing.
+* Your right to data portability - You have the right to ask that we transfer the personal information you gave us to another organisation, or to you. Read more about the right to data portability.
+* Your right to withdraw consent – When we use consent as our lawful basis you have the right to withdraw your consent at any time. Read more about the right to withdraw consent.
+If you make a request, we must respond to you without undue delay and in any event within one month.
+
+To make a data protection rights request, please contact us using the contact details at the top of this privacy notice.
+
+Our lawful bases for the collection and use of your data
+Our lawful bases for collecting or using personal information to provide services and goods are:
+* Legitimate interests – we’re collecting or using your information because it benefits you, our organisation or someone else, without causing an undue risk of harm to anyone. All of your data protection rights may apply, except the right to portability. Our legitimate interests are:
+For more information on our use of legitimate interests as a lawful basis you can contact us using the contact details set out above.
+Our lawful bases for collecting or using personal information for the operation of customer accounts and guarantees are:
+* Legitimate interests – we’re collecting or using your information because it benefits you, our organisation or someone else, without causing an undue risk of harm to anyone. All of your data protection rights may apply, except the right to portability. Our legitimate interests are:
+For more information on our use of legitimate interests as a lawful basis you can contact us using the contact details set out above.
+Our lawful bases for collecting or using personal information for service updates or marketing purposes are:
+* Legitimate interests – we’re collecting or using your information because it benefits you, our organisation or someone else, without causing an undue risk of harm to anyone. All of your data protection rights may apply, except the right to portability. Our legitimate interests are:
+For more information on our use of legitimate interests as a lawful basis you can contact us using the contact details set out above.
+Where we get personal information from
+* CCTV footage or other recordings
+* Publicly available sources
+How long we keep information
+[Insert information about how long you store personal information here.]
+
+[You said you didn't have a retention schedule, or you weren't sure. Once you've downloaded or copy and pasted this privacy notice, you can fill the information in above. You must not publish the privacy notice without it.
+
+For more information, see our guidance on how long you should store information (opens in a new window).]
+
+Who we share information with
+Others we share personal information with
+* Publicly on our website, social media or other marketing and information media
+How to complain
+
+If you have any concerns about our use of your personal data, you can make a complaint to us using the contact details at the top of this privacy notice.
+
+If you remain unhappy with how we’ve used your data after raising a complaint with us, you can also complain to the ICO.
+
+The ICO’s address:           
+
+Information Commissioner’s Office
+Wycliffe House
+Water Lane
+Wilmslow
+Cheshire
+SK9 5AF
+
+Helpline number: 0303 123 1113
+
+Website: https://www.ico.org.uk/make-a-complaint
+
+Last updated
+`
---
+++ frontend/app/privacy/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { privacy } from '../components/privacy'
+
+export default function page() {
+  return (
+    <div>{privacy}</div>
+  )
+}
