e725e678a8239bc1bae46f196a827c48351c767a
Aiden Taylor <ataylor3008@gmail.com>
2026-04-28 01:11:07 +0100

    Analytics tracking?

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -75,17 +76,22 @@ export const metadata: Metadata = {
   },
 };
 
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+const gaId = process.env.NEXT_PUBLIC_GA_ID;
+
+
   return (
     <html
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">{children}</body>
+          {gaId && <GoogleAnalytics gaId={gaId} />}
     </html>
   );
 }
